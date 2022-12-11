8a8690294ce18cb4b09bf1ec439e82fbc605d2ac
leightonguang <leighton.guang@gmail.com>
2022-12-11 13:56:00 +0800

    change name of command

--- src/commands/creator.js
+++ src/commands/creator.js
@@ -2,9 +2,9 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("leighton")
-    .setDescription("Founder of R0ADX B0T"),
+    .setName("creator")
+    .setDescription("creator of R0ADX B0T"),
   async execute(interaction) {
-    await interaction.reply("Founder of R0ADX B0T");
+    await interaction.reply("Leighton");
   },
 };
