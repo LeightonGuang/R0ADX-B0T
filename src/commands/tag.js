230881f0312e9ce1affac635695643265e766988
leightonguang <leighton.guang@gmail.com>
2022-12-12 15:30:52 +0800

    cheese touch updated

--- src/commands/tag.js
+++ src/commands/tag.js
@@ -12,38 +12,49 @@ module.exports = {
     let role = interaction.guild.roles.cache.find(x => x.name === roleName);
     let sender = interaction.member;
     let target = interaction.options.getMember('user');
-    console.log("LOG: \t sender: " + sender.user.username);
+    console.log("LOG: \t role: " + role);
 
     //if the role does not exist
     if (role == undefined) {
       //create new role
-      interaction.guild.roles.create({ name: "cheese touch" });
+      interaction.guild.roles.create({ name: roleName });
       console.log("LOG: \t created new cheese touch role");
-      await interaction.reply({ content: "set cheese touch role to game starter", ephemeral: true });
+      await interaction.reply({ content: "created new cheese touch role", ephemeral: true });
       return;
+
+      // the role exist  
+    } else {
+      //if no one have the role
+      console.log("LOG: \t exist but no one have role");
+      if (!interaction.member.roles.cache.find(r => r.name === roleName)) {
+        sender.roles.add(role);
+        await interaction.reply({ content: "Set cheese touch to yourself", ephemeral: true });
+        console.log("LOG: \t set cheese touch to yourself")
+        return;
+      }
     }
 
-    //if the user has cheese touch
+    //if the sender has cheese touch
     if (interaction.member.roles.cache.get(role.id)) {
 
-      //if new tag alrady have cheese touch
+      //if the target alrady has cheese touch
       if (target.roles.cache.some(role => role.name === "cheese touch")) {
         await interaction.reply({ content: `${target} already have cheese touch`, ephemeral: true });
         console.log(`LOG: \t ${target.user.username} already have cheese touch`);
         return;
 
       } else {
-        //target user has no cheese touch
+        //target has no cheese touch
         sender.roles.remove(role);
         console.log("LOG: \t cheese touch removed fro yourself");
         target.roles.add(role);
         console.log(`LOG: \t cheese touch given to ${target.user.username}`);
-        //give cheese touch secretly
+        //giving cheese touch successful
         await interaction.reply({ content: `cheese touch removed from yourself and given to ${target}`, ephemeral: true });
       }
 
     } else {
-      //user has no cheese touch
+      //sender has no cheese touch
       await interaction.reply({ content: `You don't have cheese touch`, ephemeral: true });
       console.log("LOG: \t You don't have cheese touch");
       return;
