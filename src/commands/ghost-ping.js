28c8d838eb16edda7b535555e66c7d2994401f45
leightonguang <leighton.guang@gmail.com>
2023-01-19 13:38:44 +0800

    ghost ping must include user and message

--- src/commands/ghost-ping.js
+++ src/commands/ghost-ping.js
@@ -4,21 +4,25 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("ghost-ping")
     .setDescription("pinging someone without their tag showing")
-    .addUserOption((option) => option.setName('user')
-      .setDescription('user'))
+    .addUserOption((option) =>
+      option
+        .setName('user')
+        .setDescription('who to ghost ping')
+        .setRequired(true),
+    )
     .addStringOption((option) =>
       option
         .setName("message")
         .setDescription("add a message")
-        .setRequired(false),
-
+        .setRequired(true),
     ),
+
   async execute(interaction) {
     let target = interaction.options.getMember('user');
     let msg = interaction.options.get("message").value;
     let spam = "||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​|| _ _ _ _ _ _"
     await interaction.reply({ content: `ghost pinging ${target}`, ephemeral: true });
     interaction.channel.send(`${msg} ${spam} ${target}`);
-    console.log(`LOG: \t ghost pinging ${target.user}`);
+    console.log(`LOG: \t ghost pinging ${target.user.tag}`);
   },
 };
